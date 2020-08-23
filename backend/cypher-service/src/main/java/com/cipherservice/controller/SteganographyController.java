39160b076905301b0c9be9c8ee5eaa21261941c3
Tesseract98 <turaev.artiom1998@yandex.ru>
2020-08-23 14:44:10 +0400

    initial microservice structure

---
+++ backend/cypher-service/src/main/java/com/cipherservice/controller/SteganographyController.java
@@ -0,0 +1,9 @@
+package com.cipherservice.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/steganography")
+public class SteganographyController {
+}
