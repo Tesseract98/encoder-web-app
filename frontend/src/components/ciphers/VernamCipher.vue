c431b10a223c91009f950c5b76ba52eb14fc2cf2
Tesseract98 <turaev.artiom1998@yandex.ru>
2021-01-01 20:25:41 +0400

    add tabs to encryption page

---
+++ frontend/src/components/ciphers/VernamCipher.vue
@@ -0,0 +1,64 @@
+<template>
+    <v-container class="fill-height">
+        <v-card width="900px" class="mx-auto px-10 py-10 mb-5" shaped>
+            <v-row>
+                <v-btn class="mr-3" color="primary" @click="onSubmit">
+                    Cipher
+                </v-btn>
+                <v-btn color="secondary" @click="decipherText = ''">
+                    Clear
+                </v-btn>
+            </v-row>
+            <v-row class="my-5">
+                <v-textarea
+                    background-color="green lighten-5"
+                    label="Enter start text"
+                    ref="decipherText"
+                    v-model="decipherText"
+                ></v-textarea>
+            </v-row>
+            <hr />
+            <v-row class="my-5">
+                <v-btn class="mr-3" color="primary" @click="onSubmit">
+                    Decipher
+                </v-btn>
+                <v-btn color="secondary" @click="cipherText = ''">
+                    Clear
+                </v-btn>
+            </v-row>
+            <v-row>
+                <v-textarea
+                    background-color="green lighten-5"
+                    label="Enter ciphered text"
+                    ref="cipherText"
+                    v-model="cipherText"
+                ></v-textarea>
+            </v-row>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+export default {
+    // beforeCreate: function () {
+    //     document.body.className = "VernamCipher";
+    // },
+    // name: "VernamCipher",
+    data() {
+        return {
+            decipherText: "",
+            cipherText: "",
+        };
+    },
+    methods: {
+        onSubmit() {
+            console.log("submit the form");
+            console.log(this.decipherText);
+            console.log(this.cipherText);
+        },
+    },
+};
+</script>
+
+<style scoped>
+</style>
--- frontend/src/views/Cipher.vue
+++ frontend/src/views/Cipher.vue
@@ -1,61 +1,40 @@
 <template>
-    <v-container class="fill-height">
-        <v-card width="900px" class="mx-auto px-10 py-10 mb-5" shaped>
-            <v-row>
-                <v-btn class="mr-3" color="primary" @click="onSubmit">
-                    Cipher
-                </v-btn>
-                <v-btn color="secondary" @click="decipherText = ''">
-                    Clear
-                </v-btn>
-            </v-row>
-            <v-row class="my-5">
-                <v-textarea
-                    background-color="green lighten-5"
-                    label="Enter start text"
-                    ref="decipherText"
-                    v-model="decipherText"
-                ></v-textarea>
-            </v-row>
-            <hr />
-            <v-row class="my-5">
-                <v-btn class="mr-3" color="primary" @click="onSubmit">
-                    Decipher
-                </v-btn>
-                <v-btn color="secondary" @click="cipherText = ''">
-                    Clear
-                </v-btn>
-            </v-row>
-            <v-row>
-                <v-textarea
-                    background-color="green lighten-5"
-                    label="Enter ciphered text"
-                    ref="cipherText"
-                    v-model="cipherText"
-                ></v-textarea>
-            </v-row>
-        </v-card>
+    <v-container>
+        <v-tabs v-model="currentTab" centered show-arrows fixed-tabs>
+            <v-tab
+                v-for="item in tabs"
+                :key="item.value"
+                :href="`#${item.value}`"
+            >
+                {{ item.name }}
+            </v-tab>
+        </v-tabs>
+
+        <v-tabs-items v-model="currentTab">
+            <v-tab-item value="VernamCipher">
+                <VernamCipher />
+            </v-tab-item>
+        </v-tabs-items>
     </v-container>
 </template>
 
 <script>
+import VernamCipher from "@/components/ciphers/VernamCipher";
+
 export default {
-    beforeCreate: function () {
-        document.body.className = "cipher";
+    components: {
+        VernamCipher,
     },
-    name: "Cipher",
+
     data() {
         return {
-            decipherText: "",
-            cipherText: "",
+            tabs: [
+                { name: "Vernam Cipher", value: "VernamCipher" },
+                { name: "Steganography", value: "Steganography" },
+                { name: "Huffman Coding", value: "HuffmanCoding" },
+            ],
+            currentTab: "VernamCipher",
         };
-    },
-    methods: {
-        onSubmit() {
-            console.log("submit the form");
-            console.log(this.decipherText);
-            console.log(this.cipherText);
-        },
     },
 };
 </script>
